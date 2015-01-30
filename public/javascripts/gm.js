8a9a5c27cf0006922d2ad8949489e2bb5b3ca854
reinbaarsma <rein@solidwebcode.com>
2015-01-30 17:41:06 +0100

    refactured model logic to seperate class

--- public/javascripts/gm.js
+++ public/javascripts/gm.js
@@ -4,34 +4,18 @@
  * @author Rein Baarsma <rein@solidwebcode.com>
  */
 
-var NPCs = function ($http) {
-    this.npcs = [];
-
-    this.update = function (npc) {
-        $http.post('/npcs/'+npc._id, npc)
-            .success(function (data) {
-                data.loading = false;
-                console.log('npc updated');
-            }).error(function(data, status){
-                console.log(data, status);
-            })
-        ;
-    };
-};
-
 angular.module('gm', [])
 
     // Service
-    .factory('NPCs', ['$http', function($http){
-        return new NPCs($http);
+    .factory('NPCCollection', ['$http', '$rootScope', function($http, $rootScope){
+        return new NPCCollection($http, $rootScope);
     }])
 
-    .controller('PanelController', ['$http', '$rootScope', 'NPCs', function($http, $rootScope, NPCs) {
+    .controller('PanelController', ['$http', '$rootScope', 'NPCCollection', function($http, $rootScope, NPCCollection) {
         var self=this;
 
         this.remove = function (index) {
-            $rootScope.npcs[index].in_panel = false;
-            NPCs.update($rootScope.npcs[index]);
+            NPCCollection.update(index, {in_panel: false});
         }
     }])
 
@@ -52,49 +36,20 @@ angular.module('gm', [])
     .directive('gmSide', function () {
         return {
             'templateUrl': 'partial/side.html',
-            'controller': ['$http', '$rootScope', 'NPCs', function($http, $rootScope, NPCs) {
+            'controller': ['$http', '$rootScope', 'NPCCollection', function($http, $rootScope, NPCCollection) {
                 console.log('fetching npcs');
                 var self=this;
-                $http.get('/npcs').success(function(data){
-                    console.log('npcs obtained');
-                    console.log(data);
-                    $rootScope.npcs = data;
-                }).error(function(data, status){
-                    console.log(data, status);
-                    $rootScope.npcs = [];
-                });
 
                 this.remove = function (index) {
-                    var id = $rootScope.npcs[index]._id;
-                    $rootScope.npcs[index].loading = true;
-
-                    $http.delete('/npcs/'+id)
-                        .success(function (data) {
-                            $rootScope.npcs.splice(index,1);
-                            console.log('npc removed');
-                        }).error(function(data, status){
-                            console.log(data, status);
-                        })
-                    ;
+                    NPCCollection.remove(index);
                 };
 
                 this.add = function (data) {
-                    var npc = {'name': self.name, loading: true};
-                    var index = $rootScope.npcs.push(npc);
-                    $http.post('/npcs', npc)
-                        .success(function (data) {
-                            data.loading = false;
-                            $rootScope.npcs[index-1] = data;
-                            console.log('npc added');
-                        }).error(function(data, status){
-                            console.log(data, status);
-                        })
-                    ;
+                    NPCCollection.add({'name': self.name});
                 };
 
                 this.addToPanel = function(index) {
-                    $rootScope.npcs[index].in_panel = true;
-                    NPCs.update($rootScope.npcs[index]);
+                    NPCCollection.update(index, {in_panel: true});
                 }
             }],
             'controllerAs': 'sideCtrl'
---
+++ public/javascripts/npc-collection.js
@@ -0,0 +1,78 @@
+/**
+ * Keeps track of a global npcs object
+ *
+ * @param $http
+ * @param $rootScope
+ * @constructor
+ */
+var NPCCollection = function ($http, $rootScope) {
+    $rootScope.npcs = [];
+
+    // helper function that always handles errors the same way
+    this.request = function (url, method, data) {
+        return $http({
+            'method': method,
+            'url': url
+        }).error(function (data, status) {
+            // TODO: show error flash or something
+            console.log(data, status);
+        })
+            ;
+    };
+
+    // load ALL npcs from /npcs
+    this.load = function () {
+        this.request('/npcs', 'GET')
+            .success(function (data) {
+                $rootScope.npcs = data;
+            })
+        ;
+    };
+
+    // update single NPC
+    this.update = function (index, data) {
+        if (typeof data != 'object')
+            throw new Error('invalid object to update npc');
+
+        data.loading = true;
+        for (var k in data) {
+            $rootScope.npcs[index][k] = data[k];
+        }
+
+        var npc = $rootScope.npcs[index];
+
+        return this.request('/npcs/' + npc._id, 'POST', npc)
+            .then(function (data) {
+                $rootScope.npcs[index].loading = false;
+                console.log('npc updated');
+            });
+        ;
+    };
+
+    // add single NPC
+    this.add = function (data) {
+        data.loading = true;
+        var index = $rootScope.npcs.push(npc);
+        return $http.request('/npcs', 'POST', npc)
+            .success(function (data) {
+                data.loading = false;
+                $rootScope.npcs[index - 1] = data;
+                console.log('npc added');
+            })
+            ;
+
+    };
+
+    // remove single NPC
+    this.remove = function (index) {
+        $rootScope.npcs[index].loading = true;
+        return this.request('/npcs/' + $rootScope.npcs[index]._id, 'DELETE')
+            .success(function (data) {
+                $rootScope.npcs.splice(index, 1);
+                console.log('npc removed');
+            });
+        ;
+    };
+
+    this.load();
+};
